c58dd1fbac04e79f3723728049698f64dc2d4b0d
amanDte21 <amandte21@gmail.com>
2023-05-04 10:46:47 +0530

    updated

--- ios/build/Pods.build/Debug-iphoneos/flutter_splash_screen.build/DerivedSources/flutter_splash_screen_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char flutter_splash_screenVersionString[];
- extern const double flutter_splash_screenVersionNumber;
-
- const unsigned char flutter_splash_screenVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:flutter_splash_screen  PROJECT:Pods-1" "\n";
- const double flutter_splash_screenVersionNumber __attribute__ ((used)) = (double)1.;
