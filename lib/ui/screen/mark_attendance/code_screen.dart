03c7daea34f10e22bdfd132a61db2c832c82e018
amanDte21 <amandte21@gmail.com>
2023-09-11 11:02:34 +0530

    updated code with version

--- lib/ui/screen/mark_attendance/code_screen.dart
+++ lib/ui/screen/mark_attendance/code_screen.dart
@@ -173,7 +173,7 @@ class CodeScreen extends StatelessWidget {
                             customMiddleTextStyle: GoogleFonts.dancingScript(),
                             text1: 'Your',
                             text3:
-                                'has been generated in the View Express Pass Section.',
+                                'has been generated in the View Express Pass Section.',
                             textColor: const Color(0xFFebaa2b),
                             fontSize: 16,
                             text:
