28d27e30df3a0a99dd43e0d9d899b0f2c795b22e
vinayakeeee <132995042+vinayakeeee@users.noreply.github.com>
2023-10-09 18:18:22 +0530

    dde

--- lib/widgets/customdrawer.dart
+++ lib/widgets/customdrawer.dart
@@ -632,7 +632,7 @@ class CustomDrawer extends StatelessWidget {
                                                     text: "Event History",
                                                     fontSize: 15,
                                                     fontWeight: FontWeight.w500,
-                                                    textColor: index == 4
+                                                    textColor: index == 9
                                                         ? ThemeConstants
                                                             .bluecolor
                                                         : ThemeConstants
