814b23bf0afaa737030fd960b0c9857e3bd1c22c
Adityamishra1234 <meaditya639@gmail.com>
2025-01-04 17:19:56 +0530

    done modification in IOS App for Gradlynk.

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.19.6/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/siecadmin/Downloads/Company Project/sp_app"
+export "FLUTTER_ROOT=/Users/adityamishra/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/adityamishra/Downloads/Company Project/sp_app 2"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=2.1.18"
